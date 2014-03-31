ff5bbe543b7a800b5f3a8258d593c9f393408e51
Kalson <Kalson.Kalu@yahoo.com>
2014-03-31 07:32:12 -0500

    Create THAppDelegate.h

---
+++ THAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THAppDelegate.h
+//  Crystalball
+//
+//  Created by KaL on 3/29/14.
+//  Copyright (c) 2014 TreeHouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
